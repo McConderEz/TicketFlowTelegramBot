4f54156664f4cbcdf514bd31d6a654bc6e0248bb
McConderEz <kamazitun.jaykehd@gmail.com>
2024-10-20 02:38:22 +0300

    init

---
+++ TelegramBotCode418Service/Features/HttpHandlers/HttpPostHandlers.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace TelegramBotCode418Service.Features.HttpHandlers;
+
+public class HttpPostHandlers
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<HttpPostHandlers> _logger;
+
+    public HttpPostHandlers(
+        IConfiguration configuration,
+        ILogger<HttpPostHandlers> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+    
+    public async Task SendPostRequestAsync(string data, CancellationToken cancellationToken)
+    {
+        using HttpClient httpClient = new HttpClient();
+
+        var url = _configuration.GetConnectionString("Url");
+        var byteArray = Encoding.UTF8.GetBytes(data);
+        
+        using var content = new ByteArrayContent(byteArray);
+        content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+        
+        using var request = new HttpRequestMessage(HttpMethod.Put, url);
+        request.Content = content;
+
+        var response = await httpClient.SendAsync(request,cancellationToken);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseData = await response.Content.ReadAsStringAsync(cancellationToken);
+            _logger.LogInformation($"Response: {responseData}");
+        }
+        else
+        {
+            _logger.LogError($"Error: {response.StatusCode}");
+        }
+    }
+}
---
+++ TelegramBotCode418Service/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Options;
+using PRTelegramBot.Core;
+using PRTelegramBot.Extensions;
+using Telegram.Bot;
+using TelegramBotCode418Service;
+using TelegramBotCode418Service.Infrastructure;
+using TelegramBotCode418Service.Options;
+
+
+const string EXIT_COMMAND = "exit";
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<TelegramBotBackgroundService>();
+
+builder.Services.AddBotHandlers();
+builder.Services.AddSingleton<ApplicationDbContext>();
+
+builder.Services.AddTransient<ITelegramBotClient, TelegramBotClient>(provider =>
+{
+    var token = provider.GetRequiredService<IOptions<TelegramOptions>>().Value.Token;
+
+    return new(token);
+});
+//builder.Services.AddSingleton<HttpPostHandlers>();
+
+builder.Services.Configure<TelegramOptions>(builder.Configuration.GetSection(TelegramOptions.Telegram));
+
+var host = builder.Build();
+
+host.Run();
+
+
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if (result.ToLower() == EXIT_COMMAND)
+        Environment.Exit(0);
+}
